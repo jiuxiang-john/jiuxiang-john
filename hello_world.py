7a3d878ed65ecf6de2ec374ca04835d7c2b04ddb
jiuxiang <57613273+jiuxiang-john@users.noreply.github.com>
2022-06-12 23:06:46 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,23 @@
-hello_world.py
-print("Hello Git world!");
+#第一章节 安装 ，第二章节 变量和简单数据类型
+message = 'gxs'
+print('Hello World!')
+print('在线图表、web页面、图形化小工具')
+print(message)
+message="name update"
+print(message)
+message='this is a "string" '
+print(message)
+print(message.title())
+print(message.upper())
+print(message.lower())
+first_name='guo'
+last_name="xiao song"
+full_name=first_name+"  "+last_name
+print("\thello,"+"\n"+full_name.title()+"!")
+favoirte_language="  python "
+print(favoirte_language.strip())
+print(favoirte_language+"xx")
+print(favoirte_language.strip()+"xx")
+print("hh"+favoirte_language+'xx')
+favoirte_language2=favoirte_language.strip().lstrip()
+print("hh"+favoirte_language2+'xx')
